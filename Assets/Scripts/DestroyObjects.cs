056359e254f862bb7233b41c9c5e77e31ae32422
SnowTelevision <pguan100@gmail.com>
2017-05-04 13:18:00 -0400

    fixing some lagging issue caused by physics and destroying objects.

---
+++ Assets/Scripts/DestroyObjects.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjects : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OnTriggerExit(Collider col)
+    {
+        print(col.name);
+        if(col.tag == "stanceDestroy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    //public void OnTriggerEnter(Collider col)
+    //{
+    //    print(col.name);
+    //    if (col.tag == "stanceDestroy")
+    //    {
+    //        Destroy(col.gameObject);
+    //    }
+    //}
+}
